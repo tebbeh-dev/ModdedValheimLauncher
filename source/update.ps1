ef86ecd935a7ad389d131e64ee14edef78a32708
tebbeh <emil@igypt.se>
2023-11-02 22:35:55 +0100

    Forgot to uncomment row

--- source/update.ps1
+++ source/update.ps1
@@ -2,7 +2,7 @@ $ProgressPreference = 'SilentlyContinue' # Do not show download progress
 
 Write-Host "Downloading latest version... " -ForegroundColor Yellow -NoNewline
 
-#Invoke-RestMethod -Uri "https://raw.githubusercontent.com/tebbeh-dev/ModdedValheimLauncher/main/source/main.ps1" -OutFile "source/main.ps1"
+Invoke-RestMethod -Uri "https://raw.githubusercontent.com/tebbeh-dev/ModdedValheimLauncher/main/source/main.ps1" -OutFile "source/main.ps1"
 Invoke-RestMethod -Uri "https://raw.githubusercontent.com/tebbeh-dev/ModdedValheimLauncher/main/source/version.json" -OutFile "source/version.json"
 
 Write-Host "OK" -ForegroundColor Green
